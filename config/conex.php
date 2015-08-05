8df06ca937dd34808b9f6ec744cb1fb71ed43d0d
academia <academi@lab01-00.(none)>
2015-08-05 13:30:58 -0430

    primeros aportes

---
+++ config/conex.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Configuration file for: Database Connection
+ * @author Angel Cruz (@abr4xas) - http://abr4xas.org
+ * mie 05 ago 2015 10:32:05 VET
+ * License: GPLv2 or later
+ * @version 0.1
+ */
+
+
+$hostname   =   '';
+$user       =   '';
+$password   =   '';
+$db_name    =   '';
+
+function CONECTAR_BD($hostname,$user,$password,$db_name){
+    function_exists('mysqli_connect') or die ('ERROR:No se tiene soporte actualmente para la base de datos MySQL');
+    global $mysqli;
+    $mysqli=mysqli_connect($hostname,$user,$password,$db_name)
+    or die ('Error inesperado, No se pudo ejecutar la instruccion. Contacte al administrador'.' '.mysqli_error($mysqli));
+    return $mysqli;
+}
+
+function QUERYBD($sql,$hostname,$user,$password,$db_name){
+$mysqli=CONECTAR_BD($hostname,$user,$password,$db_name);
+ if ($resulta=@mysqli_query($mysqli,$sql)){
+        return $resulta;
+    }else{
+        die ('Error inesperado, no se pudo ejecutar la instrucción. Contacte al administrador!');
+    }
+    return;
+}
+
+//    $sql = sprintf("SELECT * FROM ... WHERE ... ");
+//    $response =  QUERYBD($sql,$hostname,$user,$password,$db_name);
+//    $row = mysqli_fetch_array($response,MYSQLI_ASSOC);
