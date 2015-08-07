6f3375ada33c664b4e6e748954b0e1cd21ebbcc9
abr4xas <me@abr4xas.org>
2015-08-07 16:37:14 -0430

    actualizado

--- include/contenido.php
+++ include/contenido.php
@@ -1,19 +1,26 @@
 <?php
 
 function CONTENIDO_PRINCIPAL($hostname,$user,$password,$db_name){
+$sql = sprintf("SELECT * FROM publicaciones ORDER BY id ASC");
+$response =  QUERYBD($sql,$hostname,$user,$password,$db_name);
+while ($row = mysqli_fetch_array($response,MYSQLI_ASSOC)) {
     echo'
-<div class="post-preview">
-    <a href="post.html">
-        <h2 class="post-title">
-            Man must explore, and this is exploration at its greatest
-        </h2>
-        <h3 class="post-subtitle">
-            Problems look mighty small from 150 miles up
-        </h3>
-    </a>
-    <p class="post-meta">Posted by <a href="#">Start Bootstrap</a> on September 24, 2014</p>
-</div>
-<hr>';
+    <div class="post-preview">
+        <a href="post.html">
+            <h2 class="post-title">
+                '.$row["titulo"].'
+            </h2>
+            <h3 class="post-subtitle">
+                '.$row["descripcion"].'
+            </h3>
+        </a>
+        <p class="post-meta">Posted by
+        <a href="#0">'.$row["id_autor"].'</a> 
+        on '.$row["fecha"].'</p>
+    </div>
+    <hr>';    
+}//fin while    
+
     return;
 }
 
